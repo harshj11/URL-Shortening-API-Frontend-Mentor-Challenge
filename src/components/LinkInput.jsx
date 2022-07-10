424c9dcc2773e086645e49e9135cff887c943444
Harsh Jain <harshjain5454@gmail.com>
2022-07-10 11:14:19 +0530

    :FEAT:SHOW error text on clicking Shorten It! button with empty text box

--- src/components/LinkInput.jsx
+++ src/components/LinkInput.jsx
@@ -1,10 +1,42 @@
 import '../scss/linkinput.scss';
+import { useState, createRef } from 'react';
 
 const LinkInput = () => {
+
+    const [textValue, setTextValue] = useState("");
+    const [errorText, setErrorText] = useState("");
+    const inputRef = createRef();
+
+    const handleClick = (event) => {
+        if(textValue.trim().length === 0) {
+            inputRef.current.classList.add("focus");
+            setErrorText("Please add a link");
+        } else {
+            if(inputRef.current.classList.contains("focus")) {
+                inputRef.current.classList.remove("focus");
+                setErrorText("");
+            }
+        }
+    }
+
+    const handleChange = (event) => {
+        setTextValue(event.target.value);
+    }
+
     return(
         <div className="link__container flex flex-ai-c">
-            <input className="link__container__input" type="text" placeholder="Shorten a link here..."/>
-            <button className="link__container__button button--cyan">Shorten It!</button>
+            <input 
+                className="link__container__input" 
+                type="text" 
+                placeholder="Shorten a link here..." 
+                onChange={handleChange} 
+                value={textValue}
+                ref={inputRef}
+            />
+            <button className="link__container__button button--cyan" onClick={handleClick}>Shorten It!</button>
+            <span className="link__container__error">
+                <em>{errorText}</em>
+            </span>
         </div>
     )
 }
